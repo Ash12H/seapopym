38f9d10d458ad6f2a70414aa7082e8485d53ad26
Ash <lehodey.jules@gmail.com>
2025-12-19 12:54:41 +0100

    Add new model to init file

--- seapopym/model/__init__.py
+++ seapopym/model/__init__.py
@@ -1,2 +1,2 @@
 from .no_transport_model import NoTransportModel, NoTransportSpaceOptimizedLightModel, NoTransportLightModel
-from .acidity_model import AcidityModel, AcidityBedModel, AcidityBedBHModel, AcidityBedBHSurvivalModel
+from .acidity_model import AcidityModel, AcidityBedModel, AcidityBedBHModel, AcidityBedBHSurvivalModel, AcidityBedBHPFTSurvivalModel
